92ff0ce90c92dff16c4cbd043da068e6f61ef753
Nicole Magpantay <20709370+magpantaynic@users.noreply.github.com>
2022-03-30 20:39:40 -0400

    Create SummativeSum.java

---
+++ SummativeSum.java
@@ -0,0 +1,40 @@
+/* 
+	Write a static method that takes in an array of ints, adds them together, and returns the result.
+	Call your new method inside a main method and print out the result for the following three example arrays:
+
+	{ 1, 90, -33, -55, 67, -16, 28, -55, 15 }
+	{ 999, -60, -77, 14, 160, 301 }
+	{ 10, 20, 30, 40, 50, 60, 70, 80, 90, 100, 110, 120, 130, 
+	140, 150, 160, 170, 180, 190, 200, -99 } 
+
+--------------------------------------------------------------------
+
+	Output:
+
+	#1 Array Sum: 42
+	#2 Array Sum: 1337
+	#3 Array Sum: 2001 
+
+
+*/
+
+
+
+import java.util.Scanner;
+import java.util.Random;
+
+
+
+public class SummativeSums{
+
+	public static void main(String[] args)
+	{
+		
+		Scanner input = new Scanner(System.in);
+		Random randomizer = new Random();
+
+	}
+
+
+
+}
