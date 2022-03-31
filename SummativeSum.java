5a34ef311675a7bb6d765e6e3a30e11d406aa286
Nicole Magpantay <20709370+magpantaynic@users.noreply.github.com>
2022-03-31 03:02:15 -0400

    Update SummativeSum.java

--- SummativeSum.java
+++ SummativeSum.java
@@ -23,14 +23,14 @@ public class SummativeSums {
 	public static void main(String[] args)
 	{
 		
-	int[] array1 = { 1, 90, -33, -55, 67, -16, 28, -55, 15 }
-	int[] array2 = { 999, -60, -77, 14, 160, 301 }
+	int[] array1 = { 1, 90, -33, -55, 67, -16, 28, -55, 15 };
+	int[] array2 = { 999, -60, -77, 14, 160, 301 };
 	int[] array3 = { 10, 20, 30, 40, 50, 60, 70, 80, 90, 100, 110, 120, 130, 
-						140, 150, 160, 170, 180, 190, 200, -99 } 
+						140, 150, 160, 170, 180, 190, 200, -99 };
 
 
 	System.out.println("#1 Array Sum: " + sum(array1));
-	System.out.println("#2 Array Sum: " + sum(array2);
+	System.out.println("#2 Array Sum: " + sum(array2));
 	System.out.println("#3 Array Sum: " + sum(array3));
 
 
