512c52a1b51309964ed224b33a5aefcc54df9a79
Nicole Magpantay <20709370+magpantaynic@users.noreply.github.com>
2022-03-30 20:39:17 -0400

    Create HealthyHearts.java

---
+++ HealthyHearts.java
@@ -0,0 +1,37 @@
+/* 
+	Make a simple calculator that asks the user for their age. Then calculate the healthy heart rate range for that age, and display it.
+
+	Their maximum heart rate should be 220 - their age.
+	The target heart rate zone is the 50 - 85% of the maximum.
+
+	--------------------------------------------------------------------
+
+	Output:
+
+	What is your age? 50
+	Your maximum heart rate should be 170 beats per minute
+	Your target HR Zone is 85 - 145 beats per minute
+
+
+*/
+
+
+
+import java.util.Scanner;
+import java.util.Random;
+
+
+
+public class HealthyHearts{
+
+	public static void main(String[] args)
+	{
+		
+		Scanner input = new Scanner(System.in);
+		Random randomizer = new Random();
+
+	}
+
+
+
+}
