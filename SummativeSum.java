261efd06ac4c75f3fe084372f562e1d8e5960cea
Nicole Magpantay <20709370+magpantaynic@users.noreply.github.com>
2022-03-31 03:00:48 -0400

    Update SummativeSum.java

--- SummativeSum.java
+++ SummativeSum.java
@@ -18,23 +18,34 @@
 
 */
 
-
-
-import java.util.Scanner;
-import java.util.Random;
-
-
-
-public class SummativeSums{
+public class SummativeSums {
 
 	public static void main(String[] args)
 	{
 		
-		Scanner input = new Scanner(System.in);
-		Random randomizer = new Random();
+	int[] array1 = { 1, 90, -33, -55, 67, -16, 28, -55, 15 }
+	int[] array2 = { 999, -60, -77, 14, 160, 301 }
+	int[] array3 = { 10, 20, 30, 40, 50, 60, 70, 80, 90, 100, 110, 120, 130, 
+						140, 150, 160, 170, 180, 190, 200, -99 } 
+
+
+	System.out.println("#1 Array Sum: " + sum(array1));
+	System.out.println("#2 Array Sum: " + sum(array2);
+	System.out.println("#3 Array Sum: " + sum(array3));
+
 
 	}
 
+	public static int sum(int[] array)
+	{
+		int sumArray = 0;
+		for(int i=0; i < array.length; i++)
+		{
+			// this line will check each position and adding as it goes on
+			sumArray += array[i];
+		}
+		return sumArray;
 
+	}
 
 }
